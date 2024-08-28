c477eb709de76a12285735c6a196501ec1d30e37
alik-chilingaryan <chill.alik@gmail.com>
2024-08-28 15:38:57 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const {MongoClient} = require("mongodb");
+const url = "mongodb://127.0.0.1:27017";
+const bodyParser = require('body-parser');
+const app = express();
+const client = new MongoClient(url);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+client.connect();
+let db = client.db("Students");
+let users = db.collection("Products");
+
+app.get("/test", async (req,res) => {
+  const products = await users.find().toArray();
+  res.send(products);
+});
+
+app.post('/test', async (req, res) => {
+  const data = req.body;
+  await users.insertOne(data);
+  res.send(data);
+});
+
+app.listen(3005);
